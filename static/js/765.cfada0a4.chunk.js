7630ba7539e309d964d661276984d2a3528d5557
IhorKaren <karenihor@gmail.com>
2023-09-13 13:17:40 +0000

    Deploying to gh-pages from @ IhorKaren/task-car-rental@54829b3c49d66aa3e718d1a6dd1350b95581afc9 🚀

---
+++ static/js/765.cfada0a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[765],{9765:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"Info"})})}}}]);
+//# sourceMappingURL=765.cfada0a4.chunk.js.map
--- static/js/765.d02b2675.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[765],{765:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"Info"})})}}}]);
-//# sourceMappingURL=765.d02b2675.chunk.js.map
